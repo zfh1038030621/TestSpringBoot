6f67115dbd2ee9d7a77c7ee54514dc87b159c659
zfh1038030621 <1038030621@qq.com>
2017-07-07 20:36:35 +0800

    mesgsmg
    mesgsmg

--- src/main/java/com/zfh/Application.java
+++ src/main/java/com/zfh/Application.java
@@ -1,3 +1,5 @@
+package com.zfh;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 /*扫描注解的范围 ，SpringBootApplication里面是包含ComponentScan这个注解的,所以要重写一下，
 要不然扫描注解范围的值就是空*/
-@ComponentScan("redis,testPropertiesOne,spring_junit")
+@ComponentScan("redis,testPropertiesOne")
 @ImportResource(locations={"classpath:cache-context.xml"})
 @EnableTransactionManagement
 public class Application {
--- src/test/java/spring_junit/springTest.java
+++ src/test/java/spring_junit/springTest.java
@@ -1,24 +1,28 @@
-package spring_test;
+package spring_junit;
 
+import com.zfh.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.apache.coyote.http11.Constants.a;
+import org.springframework.test.context.web.WebAppConfiguration;
+import testPropertiesOne.TestProOne;
 
 /**
  * Created by zhangfanghu on 2017/5/25.
  * 使用spring-test进行单元测试
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes= Application.class)// 指定spring-boot的启动类
+
 public class springTest {
+    @Autowired
+    TestProOne testProOne;
     @Test
     public void test(){
 
-        System.out.println("12");
+        System.out.println(testProOne.getName());
     }
 }
